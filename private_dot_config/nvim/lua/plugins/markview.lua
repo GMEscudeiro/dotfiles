ca283c39b53852d8b9a67c1d72dc6637faca54be
GMEscudeiro <guimescudeiro@gmail.com>
2026-02-18 15:31:01 -0300

    Alteracoes

--- private_dot_config/nvim/lua/plugins/markview.lua
+++ private_dot_config/nvim/lua/plugins/markview.lua
@@ -1,3 +1,11 @@
-return{
-  "OXY2DEV/markview.nvim",
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
