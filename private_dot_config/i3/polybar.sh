7379bd739077ea5da32dcd759162c030e65140fd
GMEscudeiro <guimescudeiro@gmail.com>
2026-02-26 09:07:12 -0300

    Different bars according to monitor, battery, systray, new workspace distribution

--- private_dot_config/i3/polybar.sh
+++ private_dot_config/i3/polybar.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-# Garante que o script saiba onde encontrar os comandos
 PATH=$PATH:/usr/local/bin
 
-# Mata instâncias anteriores
 killall -q polybar
 
-# Espera até que os processos tenham sido encerrados
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 sleep 1
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload --config=$HOME/.config/polybar/config.ini mybar >> /tmp/polybar.log 2>&1 &
+    is_primary=$(xrandr --query | grep "$m" | grep "primary")
+    if [ ! -z "$is_primary" ]; then
+      MONITOR=$m polybar --reload pbar &
+    else
+      MONITOR=$m polybar --reload sbar &
+    fi
   done
 else
-  polybar --reload --config=$HOME/.config/polybar/config.ini mybar >> /tmp/polybar.log 2>&1 &
+  polybar --reload pbar &
 fi
